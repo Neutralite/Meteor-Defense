ccf181fa1d24ad6c89c2671c8ad0e2105ad7d8a3
Michael Law <michael.law@ontariotechu.net>
2023-04-12 16:37:34 -0400

    Update SoundManager.cs

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -8,6 +8,16 @@ public class SoundManager : MonoBehaviour
     [SerializeField]
     Slider volumeSlider;
 
+    void Start()
+    {
+        
+    }
+
+    public void SetValue(float value)
+    {
+        AudioListener.volume = value;
+        volumeSlider.value = value;
+    }
     public void ChangeVolume()
     {
         AudioListener.volume = volumeSlider.value;
