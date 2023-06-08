58882d6c63f2d4c55ba40a24a1f8b6805655f6fe
Michael Law <michael.law@ontariotechu.net>
2023-06-08 18:47:21 -0400

    Prefab Changes

--- Assets/Scripts/Game Elements/Meteor.cs
+++ Assets/Scripts/Game Elements/Meteor.cs
@@ -9,37 +9,37 @@ public class Meteor : MonoBehaviour
     public int size = 1;
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer == GameManager2.instance.shieldLayer)
-        {
-            SupportFunctions.MoveBetweenLists(GameManager2.instance.targetedCities, GameManager2.instance.untargetedCities, targetCity);
-            ScoreManager.instance.Score += 5;
-            return;
-        }
+        //if (collision.gameObject.layer == GameManager2.instance.shieldLayer)
+        //{
+        //    SupportFunctions.MoveBetweenLists(GameManager2.instance.targetedCities, GameManager2.instance.untargetedCities, targetCity);
+        //    ScoreManager.instance.Score += 5;
+        //    return;
+        //}
 
-        if (collision.gameObject.layer == GameManager2.instance.planetLayer)
-        {
-            SupportFunctions.MoveBetweenLists(GameManager2.instance.targetedCities, GameManager2.instance.untargetedCities, targetCity);
+        //if (collision.gameObject.layer == GameManager2.instance.planetLayer)
+        //{
+        //    SupportFunctions.MoveBetweenLists(GameManager2.instance.targetedCities, GameManager2.instance.untargetedCities, targetCity);
 
-            scoreChangeID = ObjectID.Meteor;
-        }
+        //    scoreChangeID = ObjectID.Meteor;
+        //}
 
-        if (collision.gameObject.layer == GameManager2.instance.cityLayer)
-        {
-            collision.gameObject.GetComponent<City>().health -= size;
+        //if (collision.gameObject.layer == GameManager2.instance.cityLayer)
+        //{
+        //    collision.gameObject.GetComponent<City>().health -= size;
 
-            if (collision.gameObject.GetComponent<City>().health <= 0)
-            {
-                MeteorDefensePoolManager.instance.ReturnObject(collision.transform.parent.gameObject, ObjectID.City);
-            }
-            else
-            {
-                SupportFunctions.MoveBetweenLists(GameManager2.instance.targetedCities, GameManager2.instance.untargetedCities, targetCity);
-            }
-            scoreChangeID = ObjectID.Meteor;
-        }
+        //    if (collision.gameObject.GetComponent<City>().health <= 0)
+        //    {
+        //        MeteorDefensePoolManager.instance.ReturnObject(collision.transform.parent.gameObject, ObjectID.City);
+        //    }
+        //    else
+        //    {
+        //        SupportFunctions.MoveBetweenLists(GameManager2.instance.targetedCities, GameManager2.instance.untargetedCities, targetCity);
+        //    }
+        //    scoreChangeID = ObjectID.Meteor;
+        //}
 
-        GetComponent<Meteor>().targetCity = null;
-        ScoreManager.instance.Score += GameManager2.instance.scoreChanges[scoreChangeID];
-        MeteorDefensePoolManager.instance.ReturnObject(transform.parent.gameObject, ObjectID.Meteor);
+        //GetComponent<Meteor>().targetCity = null;
+        //ScoreManager.instance.Score += GameManager2.instance.scoreChanges[scoreChangeID];
+        //MeteorDefensePoolManager.instance.ReturnObject(transform.parent.gameObject, ObjectID.Meteor);
     }
 }
